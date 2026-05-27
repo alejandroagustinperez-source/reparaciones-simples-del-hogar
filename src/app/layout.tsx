6559fd9289710e7a88288b2471f13b36c4b26dfe
Alejandro Perez <alejandro.agustin.perez@gmail.com>
2026-05-26 22:39:25 -0300

    feat: agregar Microsoft Clarity analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -49,6 +49,13 @@ export default function RootLayout({
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3023638239005262"
           crossOrigin="anonymous"
         />
+        <script type="text/javascript">
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "wxf55hn3w1");
+        </script>
       </head>
       <body>
         <Navbar />
